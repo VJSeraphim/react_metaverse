aa419228efa360526cccd208c769d1a3d33fde94
VJS <minsung_96@naver.com>
2022-12-28 23:58:46 +0900

    About this Metaverse Site

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,33 @@
 'use client';
 
+import { motion } from "framer-motion";
+import { TypingText } from '../components'
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
 const About = () => (
-  <section>
-    About section
+  <section classNAme={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25}}
+        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      >
+        <TypingText 
+          title="| About Metaversus"
+          textStyles="text-center"
+        />
+        <motion.p
+          variants={fadeIn('up', 'tween', 0.2, 1)}
+          className="mt-[8px] font-normal sm:text-[32px] text-[24px] text-center text-secondary-white"
+        >
+          
+        </motion.p>
+      </motion.div>
+    </div>
   </section>
 );
 
