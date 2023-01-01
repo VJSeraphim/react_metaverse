4664982496279935837d0ee538a581d154faaf9e
VJS <minsung_96@naver.com>
2023-01-01 23:59:09 +0900

    Get Started & Explore

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,36 @@
-'use client';
+'use client'
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { StartSteps, TitleText, TypingText } from "../components";
+
+import { startingFeatures } from '../constants'
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} flex-col lg:flex-row mx-auto flex gap-8`}
+    >
+      <motion.div
+        variants={ planetVariants('left') }
+        className={`${styles.flexCenter} flex-1`}
+      >
+        <img 
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+      <motion.div>
+        
+      </motion.div>
+    </motion.div>
   </section>
 );
 
