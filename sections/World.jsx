990d03ad8aa1a95a0b81ba1c397af4915d8cdb16
VJS <minsung_96@naver.com>
2023-01-04 23:57:15 +0900

    World Section Init

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { TitleText, TypingText } from "../components";
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} flex-col lg:flex-row mx-auto flex gap-8`}
+    >
+      <TypingText 
+        title="| People on the World"
+        textStyles="text-center"
+      />
+      <TitleText 
+        title={<>
+          Track friends around you and intive them to play together in the same world
+        </>}
+      />
+    </motion.div>
   </section>
 );
 
